c3821772aba61a730e043799cfdc8b3469a1268b
Engin Sonat <enginsonat86@hotmail.com>
2021-09-24 21:41:51 +0300

    Added Model and Mock Repository

--- src/CommandAPI/Models/Command.cs
+++ src/CommandAPI/Models/Command.cs
@@ -2,14 +2,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CommandAPI.Models
 {
-    [Key]
-    [Required]
-    public int Id{get;set;}
-    [Required]
-    [MaxLength(250)]
-    public string HowTo{get;set;}
-    [Required]
-    public string Platform{get;set;}
-    [Required]
-    public string CommandLine{get;set;}
+    public class Command
+    {
+        [Key]
+        [Required]
+        public int Id{get;set;}
+        [Required]
+        [MaxLength(250)]
+        public string HowTo{get;set;}
+        [Required]
+        public string Platform{get;set;}
+        [Required]
+        public string CommandLine{get;set;}
+    }   
 }
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CommandAPI.Data;
 
 namespace CommandAPI
 {
@@ -15,6 +16,7 @@ namespace CommandAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ICommandAPIRepo,MockCommandAPIRepo>();
         }
 
         public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
