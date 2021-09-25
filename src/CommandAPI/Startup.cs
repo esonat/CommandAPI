c954dd8edf57551a507cb93b62f93f2a8129c341
Engin Sonat <enginsonat86@hotmail.com>
2021-09-25 20:03:30 +0300

    Added SqlCommandAPIRepo

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -8,15 +8,23 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CommandAPI.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration{get;}
+
+        public Startup(IConfiguration configuration){
+            Configuration=configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>(opt=>opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
             services.AddControllers();
-            services.AddScoped<ICommandAPIRepo,MockCommandAPIRepo>();
+            services.AddScoped<ICommandAPIRepo,SqlCommandAPIRepo>();
         }
 
         public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
